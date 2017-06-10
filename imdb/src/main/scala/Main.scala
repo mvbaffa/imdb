0a8c254916bc6a1de246e5fa2c73da3a2b560828
Marcus Baffa <mbaffa@stone.com.br>
2017-06-10 20:50:10 -0300

    application setup

---
+++ imdb/src/main/scala/Main.scala
@@ -0,0 +1,6 @@
+object Main extends App {
+
+  import config.ServiceSettings._
+
+  log.info(s"\n * \n * Imdb API Application started \n * \n")
+}
---
+++ imdb/src/main/scala/config/Settings.scala
@@ -0,0 +1,32 @@
+package config
+
+
+import akka.actor.ActorSystem
+import akka.event.{Logging, LoggingAdapter}
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+
+trait ServiceSettings {
+
+  /*
+  * Application
+  * application.conf
+  * */
+
+  lazy private val configApp = ConfigFactory.load()
+
+  /*
+  * Application
+  * Actors
+  * */
+
+  implicit val actorSystem: ActorSystem = ActorSystem("imdb-api-actorSystem")
+  implicit val executionContext = actorSystem.dispatcher
+  implicit val log: LoggingAdapter = Logging(actorSystem, getClass)
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  log.info(s"\n * \n * Service Settings Ready \n * \n")
+
+}
+
+object ServiceSettings extends ServiceSettings
