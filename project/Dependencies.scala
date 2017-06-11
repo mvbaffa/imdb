369aab940bfe937fc4abac8ac5d48e4a3058ccd0
Marcus Baffa <mbaffa@stone.com.br>
2017-06-10 21:31:13 -0300

    akka http dependencies included

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,15 +2,23 @@ import sbt._
 
 object Dependencies{
 
+  //Dependencies Version Declarations
+  val scalaTestVersion         = "2.2.6"
+  val logBackVersion           = "1.1.7"
+  val janinoVersion            = "3.0.6"
+  val akkaActorVersion         = "2.4.11"
+  val akkaStreamVersion        = "2.5.2"
+  val akkaHttpVersion          = "10.0.7"
+
   private[this] val dependencies: Seq[ModuleID] = Seq(
-    "io.scalac"               %% "reactive-rabbit"          % "1.1.2",
-    "com.typesafe.akka"       %% "akka-stream"              % "2.4.8",
-    "com.github.nscala-time"  %% "nscala-time"              % "2.12.0",
-    "org.scalatest"           %% "scalatest"                % "2.2.6",
-    "com.typesafe.play"       %% "play-json"                % "2.5.2",
-    "com.typesafe.play"       %% "play-ws"                  % "2.5.2",
-    "ch.qos.logback"           % "logback-classic"          % "1.1.7",
-    "org.codehaus.janino"      % "janino"                   % "3.0.6"
+    "org.scalatest"           %% "scalatest"                 % scalaTestVersion,
+    "ch.qos.logback"           % "logback-classic"           % logBackVersion,
+    "org.codehaus.janino"      % "janino"                    % janinoVersion,
+    "com.typesafe.akka"        % "akka-actor_2.11"           % akkaActorVersion,
+    "com.typesafe.akka"        % "akka-stream_2.11"          % akkaStreamVersion,
+    "com.typesafe.akka"        % "akka-http-core_2.11"       % akkaHttpVersion,
+    "com.typesafe.akka"        % "akka-http_2.11"            % akkaHttpVersion,
+    "com.typesafe.akka"        % "akka-http-spray-json_2.11" % akkaHttpVersion
   )
 
   val generalDependencies: Seq[ModuleID] = dependencies
