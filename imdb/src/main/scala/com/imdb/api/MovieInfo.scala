d3afcd72433b9c7113cb593937cfc92ecddc30c0
Marcus Baffa <mbaffa@stone.com.br>
2017-06-11 15:37:11 -0300

    ResponseInfo included

--- imdb/src/main/scala/com/imdb/api/MovieInfo.scala
+++ imdb/src/main/scala/com/imdb/api/MovieInfo.scala
@@ -3,12 +3,14 @@ package com.imdb.api
 import spray.json.DefaultJsonProtocol
 
 case class MovieRegister(imdbId: String, availableSeats: Int, screenId: String)
+case class MovieReservation(imdbId: String, screenId: String)
 case class MovieInfo(imdbId: String, var availableSeats: Int, var reservedSeats: Int, screenId: String, var movieTitle: String)
+case class ReturnInfo(var operationStatus: Boolean, var msg: String, imdbId: String, var availableSeats: Int,
+                      var reservedSeats: Int, screenId: String, var movieTitle: String)
 
-object MovieInfoProtocol extends DefaultJsonProtocol {
+object MovieProtocol extends DefaultJsonProtocol {
   implicit val movieInfoFormat = jsonFormat5(MovieInfo.apply)
-}
-
-object MovieRegisterProtocol extends DefaultJsonProtocol {
   implicit val movieRegisterformat = jsonFormat3(MovieRegister.apply)
+  implicit val MovieReservationformat = jsonFormat2(MovieReservation.apply)
+  implicit val ReturnInfoformat = jsonFormat7(ReturnInfo.apply)
 }
--- imdb/src/main/scala/com/imdb/api/TickerReservation.scala
+++ imdb/src/main/scala/com/imdb/api/TickerReservation.scala
@@ -1,33 +1,41 @@
 package com.imdb.api
 
+import spray.json._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import com.imdb.config.AppSettings._
-import MovieRegisterProtocol._
-import MovieInfoProtocol._
+import MovieProtocol._
 import com.redis.RedisClient
 
 trait RestApi {
 
-  def registerMovie(movieRegister: MovieRegister): MovieInfo
-  def getMovie(movieId: String): MovieInfo
+  def registerMovie(movieRegister: MovieRegister): ReturnInfo
+  def reserveMovie(movieReservation: MovieReservation): ReturnInfo
+  def getMovie(movieId: String, screenId: String): ReturnInfo
+  def fetchMovie(movieId: String, screenId: String): MovieInfo
 
   val route =
     pathPrefix("movies") {
-      path("All") {
+      path("") {
         get {
-          complete("Movie Get called")
+          complete("Movie Reservation")
         }
       } ~
-      (get & path(Segment)) { id =>
+      (get & parameters('id, 'screen)) { (imdbId, screenId) =>
         complete {
-          getMovie(id)
+          getMovie(imdbId, screenId)
         }
       } ~
       path("Register") {
         (post & entity(as[MovieRegister])) { register =>
           val movieInfo = registerMovie(register)
+          complete(movieInfo)
+        }
+      } ~
+      path("Reserve") {
+        (post & entity(as[MovieReservation])) { reservation =>
+          val movieInfo = reserveMovie(reservation)
           complete(movieInfo)
         }
       } ~
@@ -44,23 +52,90 @@ class TickerReservation(portNumber: Int) extends RestApi {
   val redis = new RedisClient("localhost", 6379)
   val bindingFuture = Http().bindAndHandle(route, "localhost", portNumber)
 
-  def registerMovie(movieRegister: MovieRegister): MovieInfo = {
+  def registerMovie(movieRegister: MovieRegister): ReturnInfo = {
 
     val movieInfo = MovieInfo(movieRegister.imdbId, movieRegister.availableSeats, 0, movieRegister.screenId, "N/A")
+    val movieJson = movieInfo.toJson.toString
 
-    redis.sadd(movieRegister.imdbId, movieInfo)
-    println(s"\n Movie Registered: ${movieInfo}")
+    redis.set(movieRegister.imdbId+movieRegister.screenId, movieJson)
+    println(s"* Register Movie - Movie Returned: ${movieInfo}\n")
 
-    movieInfo
+    val returnInfo = ReturnInfo(true, "Operation Succeeded", movieInfo.imdbId, movieInfo.availableSeats, 0,
+      movieInfo.screenId, movieInfo.movieTitle)
+    returnInfo
   }
 
-  def getMovie(movieId: String): MovieInfo = {
+  // localhost:8080/movies?id=tt0111162&screen=san francisco movies
+  def fetchMovie(movieId: String, screenId: String): MovieInfo = {
 
-    val redisInfo = redis.smembers(movieId).get
-    println(s"\n Movie Returned: ${redisInfo}")
+    val redisInfo = redis.get(movieId + screenId)
 
-    MovieInfo("N/A", 0, 0, "N/A", "N/A")
-//    val movieInfo = MovieInfo(redisInfo.imdbId, redisInfo.availableSeats, 0, movieRegister.screenId, "N/A")
+    redisInfo match {
+
+      case Some(i) => {
+        println(s"* GetMovie - Movie Returned: ${movieId}, ${screenId}\n")
+        i.parseJson.convertTo[MovieInfo]
+      }
+
+      case None => {
+        println(s"* GetMovie - Movie ${movieId}, ${screenId} not found\n")
+        MovieInfo("N/A", 0, 0, "N/A", "N/A")
+      }
+    }
+  }
+
+  // localhost:8080/movies?id=tt0111162&screen=san francisco movies
+  def getMovie(movieId: String, screenId: String): ReturnInfo = {
+
+//    var returnInfo: ReturnInfo = ???
+    val movieInfo = fetchMovie(movieId, screenId)
+
+    movieInfo.imdbId match {
+
+      case "N/A" => {
+        println(s"* GetMovie - Movie ${movieId}, ${screenId} not found\n")
+        val movieInfo = MovieInfo("N/A", 0, 0, "N/A", "N/A")
+        ReturnInfo(false, "Movie not found", movieInfo.imdbId, movieInfo.availableSeats, 0,
+          movieInfo.screenId, movieInfo.movieTitle)
+      }
+
+      case _ => {
+        println(s"* GetMovie - Movie Returned: ${movieId}, ${screenId}\n")
+        ReturnInfo(true, "Operation Succeeded", movieInfo.imdbId, movieInfo.availableSeats, 0,
+          movieInfo.screenId, movieInfo.movieTitle)
+      }
+    }
+  }
+
+  def reserveMovie(movieReservation: MovieReservation): ReturnInfo = {
+
+    var returnInfo = getMovie(movieReservation.imdbId, movieReservation.screenId)
+
+    returnInfo.operationStatus match {
+      case true => {
+        println(s"* ReserveMovie - Movie Returned: ${returnInfo.imdbId}, ${returnInfo.screenId}")
+      }
+      case false => {
+        println(s" * ReserveMovie - Movie ${movieReservation.imdbId}, ${movieReservation.screenId} not found\n")
+        return returnInfo
+      }
+    }
+
+    if(returnInfo.reservedSeats == returnInfo.availableSeats) {
+      println(s"* ReserveMovie - No Seats Available")
+      returnInfo.operationStatus = false
+      returnInfo.msg = "No Seats Available"
+      return returnInfo
+    } else {
+      returnInfo.reservedSeats += 1
+    }
+
+    val saveInfo = MovieInfo(returnInfo.imdbId, returnInfo.availableSeats, returnInfo.reservedSeats, returnInfo.screenId, returnInfo.movieTitle)
+    val saveJson = saveInfo.toJson.toString
+
+    redis.set(returnInfo.imdbId+returnInfo.screenId, saveJson)
+
+    returnInfo
   }
 
   def shutDown: Unit = {
